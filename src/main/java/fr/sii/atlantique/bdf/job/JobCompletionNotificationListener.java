6c8c6cd7838f429ee7ce8d194366c1a222c61d36
Matthieu Jaulin <mjaulin@sii.fr>
2018-02-10 18:36:57 +0100

    Clean tmp directory after job

--- src/main/java/fr/sii/atlantique/bdf/job/JobCompletionNotificationListener.java
+++ src/main/java/fr/sii/atlantique/bdf/job/JobCompletionNotificationListener.java
@@ -1,12 +1,17 @@
 package fr.sii.atlantique.bdf.job;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
@@ -25,6 +30,11 @@ public class JobCompletionNotificationListener extends JobExecutionListenerSuppo
             LOGGER.info("!!! JOB FINISHED! Time to verify the results");
             int count = jdbcTemplate.queryForObject("SELECT count(*) FROM people", Integer.class);
             LOGGER.info("NB {}", count);
+            try {
+                FileUtils.cleanDirectory(new FileSystemResource("src/main/resources/input/partitioner").getFile());
+            } catch (IOException e) {
+                LOGGER.error("Error while cleaning working directory", e);
+            }
         }
     }
 }
